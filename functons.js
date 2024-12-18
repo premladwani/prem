1ff7d8f42414a8e74807100aec4c825955ef1e90
premladwani <158132311+premladwani@users.noreply.github.com>
2024-12-18 20:19:10 +0530

    Add files via upload

---
+++ functons.js
@@ -0,0 +1,24 @@
+// Length
+let str="hello world";
+console.log(str.length);
+// indexOf
+console.log("index:",str.indexOf("world"));
+// lastIndexOf
+let string="hello world world world";
+console.log("last index is:",string.lastIndexOf("world"));
+// slice
+console.log(str.slice(0,5));
+// substring
+console.log(str.substring(0,4));
+//replace
+console.log(str.replace("world","javascript"));
+//split
+console.log(str.split(","));
+//trim
+let strr="   hello world   ";
+console.log(strr.trim());
+//touppercase
+console.log(str.toUpperCase());
+//tolowercase
+let strrr="HELLO WORLD";
+console.log(strrr.toLowerCase());
