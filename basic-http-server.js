e8aa9cd6f8e19dce3af86fe91656a41a0c05405f
premladwani <158132311+premladwani@users.noreply.github.com>
2024-12-31 20:08:11 +0530

    Update basic-http-server.js

--- basic-http-server.js
+++ basic-http-server.js
@@ -6,3 +6,6 @@ app.get('/',function(req,res){
     res.send('hello world!')
 })
 app.listen(port)
+// get request is used to get some data,post req is used to post something like posting an img in instagram,put is used to replace the data
+//delete is used to delete the data
+//here are some status code 200:everything is ok,404:file not found,411:the inputs are incorrect,500:there is an error
